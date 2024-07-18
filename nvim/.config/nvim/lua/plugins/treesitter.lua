e4dca0e8e42976158da6a58ef7464b5c3d8882d9
Maverick-Chow <chowshmaverick@gmail.com>
2024-07-18 23:11:14 +0800

    treesitter incremental selection added

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,37 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
+  event = { "BufReadPre", "BufNewFile" },
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = {"lua", "haskell"},
       highlight = { enable = true },
       indent = { enable = true },
+      ensure_installed = {
+        "lua",
+        "haskell",
+        "c",
+        "css",
+        "dockerfile",
+        "git_config",
+        "git_rebase",
+        "gitignore",
+        "html",
+        "kotlin",
+        "java",
+        "markdown",
+        "markdown_inline",
+        "sql",
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        }
+      },
     })
   end
 }
