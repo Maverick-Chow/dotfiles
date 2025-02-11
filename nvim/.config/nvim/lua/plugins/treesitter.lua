bc3f86668ce7ddfd134ecca133cbb56ad3f5429a
Maverick-Chow <chowshmaverick@gmail.com>
2025-02-11 23:26:55 +0000

    feat (nvim): added treesitter text object

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,38 +1,95 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      highlight = { enable = true },
-      indent = { enable = true },
-      ensure_installed = {
-        "lua",
-        "haskell",
-        "c",
-        "cpp",
-        "css",
-        "dockerfile",
-        "git_config",
-        "git_rebase",
-        "gitignore",
-        "html",
-        "kotlin",
-        "java",
-        "markdown",
-        "markdown_inline",
-        "sql",
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local config = require("nvim-treesitter.configs")
+      config.setup({
+        highlight = { enable = true },
+        indent = { enable = true },
+        ensure_installed = {
+          "lua",
+          "haskell",
+          "c",
+          "cpp",
+          "css",
+          "dockerfile",
+          "git_config",
+          "git_rebase",
+          "gitignore",
+          "html",
+          "kotlin",
+          "java",
+          "markdown",
+          "markdown_inline",
+          "sql",
+          "scala",
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-space>",
+            node_incremental = "<C-space>",
+            scope_incremental = false,
+            node_decremental = "<bs>",
+          }
+        },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ['aa'] = '@parameter.outer',
+              ["ia"] = "@parameter.inner",
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+              ["ii"] = "@conditional.inner",
+              ["ai"] = "@conditional.outer",
+              ["al"] = "@loop.outer",
+              ["il"] = "@loop.inner",
+              ["at"] = "@comment.outer"
+            },
+          },
+          move = {
+            enable = true,
+            set_jumps = true,
+            goto_next_start = {
+              ["]f"] = "@function.outer",
+              ["]c"] = "@class.outer",
+              ["]a"] = "@parameter.outer",
+              ["]i"] = "@conditional.outer",
+              ["]l"] = "@loop.outer",
+            },
+            goto_next_end = {
+              ["]F"] = "@function.outer",
+              ["]C"] = "@class.outer",
+              ["]A"] = "@parameter.outer",
+              ["]I"] = "@conditional.outer",
+              ["]L"] = "@loop.outer",
+            },
+            goto_previous_start = {
+              ["[f"] = "@function.outer",
+              ["[c"] = "@class.outer",
+              ["[a"] = "@parameter.outer",
+              ["[i"] = "@conditional.outer",
+              ["[l"] = "@loop.outer",
+            },
+            goto_previous_end = {
+              ["[F"] = "@function.outer",
+              ["[C"] = "@class.outer",
+              ["[A"] = "@parameter.outer",
+              ["[I"] = "@conditional.outer",
+              ["[L"] = "@loop.outer",
+            },
+          }
         }
-      },
-    })
-  end
+      })
+    end
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  }
 }
