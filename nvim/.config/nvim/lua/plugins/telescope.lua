c3e8ff97361659bbb3d272df133a472f30202af2
Maverick-Chow <chowshmaverick@gmail.com>
2025-09-03 23:42:49 +0100

    refactor (nvim): fixed telescope.lua to match lazy recomended structure

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -6,56 +6,53 @@ return {
     config = function()
       local builtin = require("telescope.builtin")
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = 'Find files' })
-      vim.keymap.set('n', '<leader>shf', function()
-        require("telescope.builtin").find_files({ hidden = true })
-      end, { desc = 'Find files (hidden)' })
-      vim.keymap.set('n', '<leader>shg', function()
-        builtin.live_grep({
-          additional_args = function(_)
-            return { "--hidden", "--glob", "!.git/*" }
-          end,
-        })
-      end, { desc = 'Search by grep (hidden)' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = 'Search current word' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = 'Search by grep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = 'Search diagnostics' })
       vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = 'Find existing buffers' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = 'Search keymaps' })
       vim.keymap.set('n', '<leader>sS', builtin.git_status, { desc = 'Search git status' })
-    end
+    end,
+    keys = {
+      { '<leader>shf',
+        function()
+          require("telescope.builtin").find_files({ hidden = true })
+        end,
+        desc = 'Find files (hidden)' },
+      { '<leader>shg',
+        function()
+          require("telescope.builtin").live_grep({
+            additional_args = function(_)
+              return { "--hidden", "--glob", "!.git/*" }
+            end,
+          })
+        end,
+        desc = 'Search grep (hidden)'
+      },
+      { '<leader>sf', function() require("telescope.builtin").find_files() end, desc = 'Find files' },
+      { '<leader>sw', function() require("telescope.builtin").grep_string() end, desc = 'Search current word' },
+      { '<leader>sg', function() require("telescope.builtin").live_grep() end, desc = 'Search by grep' },
+      { '<leader>sd', function() require("telescope.builtin").diagnostics() end, desc = 'Search diagnostics' },
+      { '<leader>sb', function() require("telescope.builtin").buffers() end, desc = 'Find existing buffers' },
+      { '<leader>sk', function() require("telescope.builtin").keymaps() end, desc = 'Search keymaps' },
+      { '<leader>sS', function() require("telescope.builtin").git_status() end, desc = 'Search git status' },
+    }
   },
   -- for autocomplete telescope select
   {
     'nvim-telescope/telescope-ui-select.nvim',
     config = function()
-      -- This is your opts table
       require("telescope").setup {
         extensions = {
           ["ui-select"] = {
             require("telescope.themes").get_dropdown {
-              -- even more opts
             }
-            -- pseudo code / specification for writing custom displays, like the one
-            -- for "codeactions"
-            -- specific_opts = {
-            --   [kind] = {
-            --     make_indexed = function(items) -> indexed_items, width,
-            --     make_displayer = function(widths) -> displayer
-            --     make_display = function(displayer) -> function(e)
-            --     make_ordinal = function(e) -> string
-            --   },
-            --   -- for example to disable the custom builtin "codeactions" display
-            --      do the following
-            --   codeactions = false,
-            -- }
           }
         },
         find_files = {
           hidden = true
         }
       }
-      -- To get ui-select loaded and working with telescope, you need to call
-      -- load_extension, somewhere after setup function:
       require("telescope").load_extension("ui-select")
     end
   },
