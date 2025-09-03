b0fd41e796664fc18087bccf1fd3aefadbe0f7ce
Maverick-Chow <chowshmaverick@gmail.com>
2025-09-03 23:09:43 +0100

    refactor (nvim): removed unnecessary lines on lsp.lua

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -22,10 +22,6 @@ return {
       "neovim/nvim-lspconfig",
     }
   },
-  -- Enable Java lsp support
-  {
-    "mfussenegger/nvim-jdtls",
-  },
   -- Enable lsp support in nvim
   {
     "neovim/nvim-lspconfig",
@@ -115,6 +111,8 @@ return {
         mapping = cmp.mapping.preset.insert({
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
           ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-e>'] = cmp.mapping.abort(),
           ['<Tab>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
@@ -130,57 +128,6 @@ return {
   -- Comment.nvim enables commenting and uncommenting code
   {
     "numToStr/Comment.nvim",
-    opts = {
-
-    },
-    lazy = false,
-    config = function()
-      require("Comment").setup(
-        {
-          ---Add a space b/w comment and the line
-          padding = true,
-          ---Whether the cursor should stay at its position
-          sticky = true,
-          ---Lines to be ignored while (un)comment
-          ignore = nil,
-          ---LHS of toggle mappings in NORMAL mode
-          toggler = {
-            ---Line-comment toggle keymap
-            line = 'gcc',
-            ---Block-comment toggle keymap
-            block = 'gbc',
-          },
-          ---LHS of operator-pending mappings in NORMAL and VISUAL mode
-          opleader = {
-            ---Line-comment keymap
-            line = 'gc',
-            ---Block-comment keymap
-            block = 'gb',
-          },
-          ---LHS of extra mappings
-          extra = {
-            ---Add comment on the line above
-            above = 'gcO',
-            ---Add comment on the line below
-            below = 'gco',
-            ---Add comment at the end of line
-            eol = 'gcA',
-          },
-          ---Enable keybindings
-          ---NOTE: If given `false` then the plugin won't create any mappings
-          mappings = {
-            ---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
-            basic = true,
-            ---Extra mapping; `gco`, `gcO`, `gcA`
-            extra = true,
-          },
-          ---Function to call before (un)comment
-          pre_hook = nil,
-          ---Function to call after (un)comment
-          post_hook = nil,
-        }
-      )
-    end
   },
   {
       'Kicamon/markdown-table-mode.nvim',
